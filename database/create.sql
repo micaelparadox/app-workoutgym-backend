f156f400dcd5e18b294f87d78e4db5712d55668d
Micael Santana <micaelparadox@gmail.com>
2022-09-09 15:04:36 -0300

    refact database

--- database/create.sql
+++ database/create.sql
@@ -3,7 +3,6 @@ create schema blog;
 create table blog.post (
     id serial primary key,
     title text not null,
-    body text not null,
-    created_at timestamp with time zone not null default now(),
-    updated_at timestamp with time zone not null default now()
+    content text not null,
+    date timestamp default now()
 );
--- server/route/postsRoute.js
+++ server/route/postsRoute.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/posts', async function (req, res) {
-    res.end();
+router.get("/posts", async function (req, res) {
+  res.json([
+    {
+      id: 1,
+      title: "First post",
+      content: "....",
+      date: new Date(),
+    },
+  ]);
 });
-router.get('/posts/:id', async function (req, res) {
-
-});
-router.post('/posts', async function (req, res) {
-
-});
-router.put('/posts/:id', async function (req, res) {
-
-});
-router.delete('/posts/:id', async function (req, res) {
-
-});
+router.get("/posts/:id", async function (req, res) {});
+router.post("/posts", async function (req, res) {});
+router.put("/posts/:id", async function (req, res) {});
+router.delete("/posts/:id", async function (req, res) {});
 
 module.exports = router;
